e6668c67d26ca60f1f4232739f71e2e9d0161326
AlexanderBollbach <alexander.bollbach@gmail.com>
2018-08-07 21:24:43 -0400

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const App = () => {
-	return <div> template </div>
+  return (
+    <div style={{ width: "100%", height: "100%", backgroundColor: "yellow" }}>
+      <Sequencer />
+    </div>
+  );
+};
+
+class Sequencer extends Component {
+  state = {
+    clock: 0
+  };
+
+  constructor() {
+    super();
+    setInterval(() => {
+      let val = this.state.clock + 1;
+      this.setState({ clock: val % 16 });
+    }, 150);
+  }
+
+  render() {
+    const { clock } = this.state;
+
+    const trackData = [new Array(16).fill(false), new Array(16).fill(false)];
+
+    const tracks = trackData.map((t, i) => {
+      return <Track key={i} clock={clock} trackData={t} />;
+    });
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "purple"
+        }}
+      >
+        {tracks}
+      </div>
+    );
+  }
 }
 
-export default App
+const Track = ({ clock, trackData }) => {
+  const divs = trackData.map((b, i) => {
+    const amount = i == 0 ? 0 : i / trackData.length;
+    const x = amount * 100;
+
+    return (
+      <div
+        key={i}
+        style={{
+          //   position: "absolute",
+          //   top: `${0}%`,
+          //   left: `${x}%`,
+          backgroundColor: i == clock ? "black" : "red",
+          flexBasis: "100%"
+          //   width: `5%`,
+          //   height: "100%"
+        }}
+      />
+    );
+  });
+  return (
+    <div
+      style={{
+        backgroundColor: "green",
+        // position: "relative",
+        display: "flex",
+        flexBasis: "100%"
+      }}
+    >
+      {divs}
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
 module.exports = {
-	entry: ["./src/index.js"],
-	output: {
-		path: __dirname + "/dist/",
-		publicPath: "/dist",
-		filename: "bundle.js"
-	},
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + "/dist/",
+    publicPath: "/dist",
+    filename: "bundle.js"
+  },
 
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015", "react", "stage-2"]
-				}
-			},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react", "stage-2"]
+        }
+      },
 
-			{
-				test: /\.css$/,
-				loader:
-					"style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
-			}
-		]
-	},
-	devServer: {
-		publicPath: "/",
-		contentBase: "./dist"		
-	}
+      {
+        test: /\.css$/,
+        loader:
+          "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+      }
+    ]
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist"
+  },
+  devtool: "source-map"
 };
